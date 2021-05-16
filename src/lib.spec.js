2bf5f5a9c70515e443558dc43ebc5b48c54a02d0
chris48s <chris.shaw480@gmail.com>
2021-05-16 12:12:25 +0100

    rewrite tests using env vars instead of mocking getInputs()

--- src/lib.spec.js
+++ src/lib.spec.js
@@ -4,13 +4,14 @@ const assert = require("assert");
 const { CoverageXml } = require("./lib");
 
 describe("CoverageXml", function () {
+  afterEach(function () {
+    delete process.env["INPUT_COVERAGE-FILE-NAME"];
+  });
+
   it("throws if line-rate is missing", async function () {
-    class CoverageXmlStub extends CoverageXml {
-      getInputs() {
-        return { coverageFileName: "./testdata/line-rate-missing.xml" };
-      }
-    }
-    const stub = new CoverageXmlStub();
+    process.env["INPUT_COVERAGE-FILE-NAME"] =
+      "./testdata/line-rate-missing.xml";
+    const stub = new CoverageXml();
     await assert.rejects(stub.render(), {
       name: "Error",
       message: "file does not contain '.coverage.@_line-rate' property",
@@ -18,12 +19,9 @@ describe("CoverageXml", function () {
   });
 
   it("throws if line-rate is not a float", async function () {
-    class CoverageXmlStub extends CoverageXml {
-      getInputs() {
-        return { coverageFileName: "./testdata/line-rate-not-float.xml" };
-      }
-    }
-    const stub = new CoverageXmlStub();
+    process.env["INPUT_COVERAGE-FILE-NAME"] =
+      "./testdata/line-rate-not-float.xml";
+    const stub = new CoverageXml();
     await assert.rejects(stub.render(), {
       name: "Error",
       message: "'.coverage.@_line-rate' property must be a float",
@@ -31,12 +29,8 @@ describe("CoverageXml", function () {
   });
 
   it("renders if coverage file is valid", async function () {
-    class CoverageXmlStub extends CoverageXml {
-      getInputs() {
-        return { coverageFileName: "./testdata/valid.xml" };
-      }
-    }
-    const stub = new CoverageXmlStub();
+    process.env["INPUT_COVERAGE-FILE-NAME"] = "./testdata/valid.xml";
+    const stub = new CoverageXml();
     const badge = await stub.render();
     assert.deepStrictEqual(
       { message: "88%", messageColor: "yellowgreen" },
